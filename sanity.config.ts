d099cf49781f190f1d49b8df0fa2dbec0193e474
Jawdat Abadullah <Jawdat.89@outlook.com>
2023-07-15 19:10:54 +0300

    install sanity and embed it into the project

---
+++ sanity.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+
+const config = defineConfig({
+  projectId: "te48geij",
+  dataset: "production",
+  title: "My Personal Website",
+  apiVersion: "2023-07-15",
+  basePath: "/admin",
+  plugins: [deskTool()],
+});
+
+export default config;
